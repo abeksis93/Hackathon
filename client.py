9f73ebbd2073d95e92add01ae469e7c2633b941a
abeksis93 <abeksis@post.bgu.ac.il>
2021-12-23 20:59:52 +0200

    main structure for server and client arch

--- client.py
+++ client.py
@@ -1,1 +1,94 @@
-print("hello client")
+import socket
+import threading
+from scapy.all import *
+
+CLIENT_IP = socket.gethostbyname(socket.gethostname())
+CLIENT_UDP_PORT = 12026
+CLIENT_TCP_PORT = 2026
+# FORMAT = 'ASCI'
+# ADDRESS = (CLIENT_IP, CLIENT_UDP_PORT)
+MAGIC_COOKIE = "0xabcddcba"
+MESSAGE_TYPE = "0x2"
+
+class Client:
+    def __init__(self):
+        """
+        explain this shit
+        """
+        self.ip = CLIENT_IP
+        try:
+            socket.inet_aton(self.ip)
+        except:
+            print("There was a problem trying to get the client's IP address.")
+        try:
+            self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        except:
+            print("There is a problem with the client's UPD socket.")
+        try:
+            self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except:
+            print("There is a problem with the client's TCP socket.")
+        self.client_name = "BotNet"
+        self.available = True
+
+    def start(self):
+        """
+        explain this shit
+        """
+        print("Client started, listening for offer requests...")
+        self.udp_socket.bind((CLIENT_IP, CLIENT_UDP_PORT))
+        # packed_message, server_address = self.udp_socket.recvfrom(1024)
+        # print(server_address)
+        # print(packed_message)
+        while True:
+            try:
+                packed_message, server_address = self.udp_socket.recvfrom(1024)
+                message = struct.unpack('Ibh', packed_message) 
+                magic_cookie = message[0]
+                message_type = message[1]
+                server_port = message[2]
+                if magic_cookie != MAGIC_COOKIE or message_type != MESSAGE_TYPE:
+                    continue
+                print("Received offer from " + server_address[0] + ", attempting to connect...")
+                try:
+                    self.tcp_socket.connect(server_address[0], server_port)
+                    print("Successfully connected to the server.\nServer's IP address: " + server_address[0])
+                except:
+                    print("Failed to connect to the server.")
+                try:
+                    self.tcp_socket.send(self.client_name + "\n")
+                except:
+                    self.tcp_socket.close()
+                    continue
+                break
+            except:
+                continue
+        self.udp_socket.close()
+
+    
+
+    def quick_math(self):
+        """
+        explain this shit
+        """
+        # stop after finishing the game
+        # self.stop() //UNCOMMENT AFTER IMPLEMENTATION
+        pass
+
+
+    def stop(self):
+        """
+        explain this shit
+        """
+        self.tcp_socket.close()
+
+def main():
+    # while True:
+    client = Client()
+    client.start()
+    # pass
+
+
+if __name__ == '__main__':
+    main()
+
--- server.py
+++ server.py
@@ -2,37 +2,106 @@ import socket
 import threading
 from scapy.all import *
 
+
+SERVER_IP = socket.gethostbyname(socket.gethostname())
+SERVER_PORT = 26262
+DEST_UDP_PORT = 12026
+FORMAT = 'ASCI'
+ADDRESS = (SERVER_IP, SERVER_PORT)
+MAGIC_COOKIE = "0xabcddcba"
+MESSAGE_TYPE = "0x2"
+
+
 class Server:
     def __init__(self):
-        # self.ip = scapy.get_if_addr("eth1")
-        self.port = "SERVER_PORT"
-        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        """
+        explain this shit
+        """        
+        self.ip = SERVER_IP
+        try:
+            socket.inet_aton(self.ip)
+        except:
+            print("There was a problem trying to get the server's IP address.")
+        self.port = SERVER_PORT
+        try:
+            self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        except:
+            print("There is a problem with the server's UPD socket.")
+        try:
+            self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except:
+            print("There is a problem with the server's TCP socket.")
+        print(self.ip)
+        self.broadcasting = False
+        self.available = False
 
-    def start_server(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((HOST, PORT))
-            s.listen()
-            conn, addr = s.accept()
-            with conn:
-                print('Connected by', addr)
-                while True:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    conn.sendall(data)
+    def start(self):
+        """
+        explain this shit
+        """
+        self.udp_socket.bind(ADDRESS)
+        self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.tcp_socket.bind(ADDRESS)
+        self.tcp_socket.listen(2)
+        self.thread_handler()
+        self.tcp_socket.close()
+        self.udp_socket.close()
 
 
-    def broadcast_to_clients(self):
+    def stop(self):
+        """
+        explain this shit
+        """
+        self.tcp_socket.close()
+        self.udp_socket.close()
+
+    def thread_handler(self):
+        """
+        explain this shit
+        """
+        broadcast_thread = threading.Thread(target=self.broadcast_handler)
+        listen_thread = threading.Thread(target=self.client_handler)
+        broadcast_thread.start()
+        listen_thread.start()
+        listen_thread.join()
+        broadcast_thread.join()
+
+
+    def client_handler(self):
+        """
+        explain this shit
+        """
+        pass
+
+    def broadcast_handler(self):
+        """
+        explain this shit
+        """
+        message = struct.pack('Ibh', MAGIC_COOKIE, MESSAGE_TYPE, self.port)
+        max_time = time.time() + 10
+        self.broadcasting = True
+        while time.time() < max_time:
+            address = ('<broadcast>', DEST_UDP_PORT)
+            self.udp_socket.sendto(message, address)
+            # print(f'Broadcast message number {counter} sent')
+            time.sleep(1)
+        self.broadcasting = False
+
+    def competition(self):
+        pass
+
+    
+    def question_generator(self):
+        pass
+
+    def calculator(self):
         pass
 
 
-
-
-
 # def main():
-#     print("hello server")
-#     print(get_if_addr("eth0"))
+    # print("hello server")
+    # print(get_if_addr(conf.iface)) # This is actually great
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    # main()
+    Server()
