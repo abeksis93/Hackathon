013100afedcdbc176ba957601be72bd26dd9d329
leahmashi <63493176+leahmashi@users.noreply.github.com>
2021-12-29 23:12:44 +0200

    new client

--- client.py
+++ client.py
@@ -1,25 +1,26 @@
 import socket
 import threading
 from scapy.all import *
-# from inputimeout import inputimeout, TimeoutOccurred
 import getch
-
+import sys, termios, tty, time, threading, random
 
 CLIENT_IP = get_if_addr('eth1')
 CLIENT_UDP_PORT = 13117
 CLIENT_TCP_PORT = 2026
-# FORMAT = 'ASCI'
-# ADDRESS = (CLIENT_IP, CLIENT_UDP_PORT)
 MAGIC_COOKIE = 0xabcddcba
 MESSAGE_TYPE = 0x2
+BUFFER = 1024
+
 
 class Client:
+    '''
+    A class representing a client with 2 sockets: UDP and TCP
+    allowing communication with a server for a simple math problems game
+    '''
+
     def __init__(self):
-        """
-        explain this shit
-        """
+        """ initiate 2 sockets: UDP and TCP for communication with server"""
         self.ip = CLIENT_IP
-        # self.ip = "132.72.202.2"
         try:
             socket.inet_aton(self.ip)
         except:
@@ -32,26 +33,24 @@ class Client:
             self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         except:
             print("There is a problem with the client's TCP socket.")
-        self.client_name = "BotNet"
+        self.client_name = "Robotrix"
         self.available = True
+        self.wasStopped = False
+
 
     def start(self):
-        """
-        explain this shit
-        """
+        """ bind the udp socket to the server and connect to the server via tcp socket """
         print("Client started, listening for offer requests...")
         self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        # self.udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.udp_socket.bind(('0.0.0.0', CLIENT_UDP_PORT))
-        # packed_message, server_address = self.udp_socket.recvfrom(1024)
-        # print(server_address)
-        # print(packed_message)
+        self.udp_socket.bind(('', CLIENT_UDP_PORT))
         while True:
             try:
                 packed_message, server_address = self.udp_socket.recvfrom(1024)
                 magic_cookie, message_type, server_port = struct.unpack('Ibh', packed_message)
                 if magic_cookie != MAGIC_COOKIE or message_type != MESSAGE_TYPE:
+                    continue
+                if server_port != 12026:
                     continue
                 server_socket = (server_address[0], server_port)
                 try:
@@ -60,51 +59,65 @@ class Client:
                 except:
                     print("Failed to connect to the server.")
                 try:
+                    print("Client trying to send name to the server.")
                     self.tcp_socket.send((self.client_name + "\n").encode())
+                    print("Client sent name to the server.")
                 except:
-                    self.tcp_socket.close()
-                    continue
+                    print("Client couldn't send name to the server.")
+                    self.stop()
+                    self.wasStopped = True
+                    break
+                try:
+                    message = self.tcp_socket.recv(BUFFER).decode()
+                    print(message)
+                except:
+                    break
                 break
             except:
                 continue
-        self.udp_socket.close()
-
-
+        
 
     def quick_math(self):
         """
-        explain this shit
+        send an answer from client to server 
         """
-        # print("In quick math")
         try:
-            message = self.tcp_socket.recv(1024).decode()
-            print(message)
-            # Timeout = 10
-            # end_time = time.time() + Timeout
-            # answer = None
-            # while time.time() < end_time or answer != None:
-            # try:
-            #     answer = inputimeout(prompt='Enter your answer: ', timeout=10)
-            # except TimeoutOccurred:
-            #     answer = ''
-            answer = getch.getch()
-            self.tcp_socket.send(answer.encode())
-            result = self.tcp_socket.recv(1024).decode()
+            
+
+            def getchar():
+            # Return a single character from stdin.
+
+                fd = sys.stdin.fileno()
+                old_settings = termios.tcgetattr(fd)
+                try:
+                    tty.setraw(sys.stdin.fileno())
+                    ch = sys.stdin.read(1)
+                finally:
+                    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+                return ch
+
+            answer = getchar()#getch.getch() #sys.stdin.readline()
+            print("your answer was: {}".foramt(str(answer)))
+            time.sleep(5)
+            print("before encode")
+            encoded_ans = str(answer).encode()
+            self.tcp_socket.sendall(encoded_ans)
+            print("after encode")
+            time.sleep(5)
+
+        finally:
+            result = self.tcp_socket.recv(BUFFER).decode()
             print(result)
-
-            # print("YOUR ANSWER IS : ", answer)
-
-            # return
-        except:
             self.stop() # stop after finishing the game
-            # return
+            self.wasStopped = True
 
     def stop(self):
         """
-        explain this shit
+        stop the cient
         """
-        self.tcp_socket.close()
-        self.udp_socket.close()
+        if self.wasStopped == False:
+            self.tcp_socket.close()
+            self.udp_socket.close()
 
 
 def main():
@@ -113,12 +126,10 @@ def main():
         client.start()
         try:
             client.quick_math()
-            # sleep(5)
+            time.sleep(1)
         finally:
-            # traceback.print_exc()
             client.stop()
             continue
-    # pass
 
 
 if __name__ == '__main__':
